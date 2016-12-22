e83e8bcd64d6e64321606e2d1c27d5e81a1858d7
Hiroto Yamakawa <cist.yamakawa@gmail.com>
2016-12-23 00:34:49 +0900

    OrderPage02までが動作するように修正

--- src/main/java/com/example/page8/form/CompletionPage.java
+++ src/main/java/com/example/page8/form/CompletionPage.java
@@ -5,13 +5,15 @@ import com.example.page8.IndexPage;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.CompoundPropertyModel;
 import org.apache.wicket.model.IModel;
 
 public class CompletionPage extends WebPage {
 
 	public CompletionPage(IModel<Order> orderModel) {
-		add(new Label("userName", orderModel.getObject()::getUserName));
-		add(new Label("dish", orderModel.getObject()::getDish));
+		setDefaultModel(CompoundPropertyModel.of(orderModel));
+		add(new Label("userName"));
+		add(new Label("dish"));
 		add(Link.onClick("toIndexPage", (link) -> setResponsePage(IndexPage.class)));
 	}
 }
